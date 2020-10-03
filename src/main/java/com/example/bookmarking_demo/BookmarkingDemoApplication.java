51691bda48d95cc695ad756679fb820331962382
Ivan Andrić <iandric@infosistem.hr>
2020-10-03 20:37:12 +0200

    Inicijalni import - kostur projekta

---
+++ src/main/java/com/example/bookmarking_demo/BookmarkingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookmarking_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmarkingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookmarking_demo/BookmarkingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookmarking_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
