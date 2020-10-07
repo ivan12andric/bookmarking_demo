8c8fc7c1ba7a3b843582bca35ed907f9e5c3c36a
Ivan Andrić <iandric@infosistem.hr>
2020-10-07 15:49:47 +0200

    Korisnik kontroler - refactoring

--- src/main/java/com/example/bookmarking_demo/controller/KorisnikController.java
+++ src/main/java/com/example/bookmarking_demo/controller/KorisnikController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,11 +19,16 @@ public class KorisnikController {
 	@Autowired
 	KorisnikService korisnikService;
 
-	@GetMapping(value = "korisnik")
+	@GetMapping(value = "/korisnici")
 	public List<Korisnik> getAll() {
 		List<Korisnik> korisnikList = korisnikService.findAll();
 
 		return korisnikList;
+	}
+
+	@PostMapping(value = "/korisnik")
+	public void save(@RequestBody Korisnik korisnik) throws Exception {
+		korisnikService.save(korisnik);
 	}
 
 }
