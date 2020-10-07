75e2dee3559ec661c5cea35c7938f1f9445b66d0
Ivan Andrić <iandric@infosistem.hr>
2020-10-07 15:49:03 +0200

    Korisnici - repo, service - nove metode, validacije, refactoring

--- src/main/java/com/example/bookmarking_demo/repository/KorisnikRepository.java
+++ src/main/java/com/example/bookmarking_demo/repository/KorisnikRepository.java
@@ -1,5 +1,8 @@
 package com.example.bookmarking_demo.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,11 @@ import com.example.bookmarking_demo.model.Korisnik;
 
 @Repository
 public interface KorisnikRepository extends JpaRepository<Korisnik, Long> {
+
+	List<Korisnik> findByAktivan(Boolean aktivan);
+
+	Optional<Korisnik> findByIdAndAktivan(Long id, Boolean aktivan);
+
+	Optional<Korisnik> findByKorisnickoImeAndAktivan(String korisnickoIme, Boolean aktivan);
 
 }
--- src/main/java/com/example/bookmarking_demo/service/KorisnikService.java
+++ src/main/java/com/example/bookmarking_demo/service/KorisnikService.java
@@ -1,8 +1,13 @@
 package com.example.bookmarking_demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.bookmarking_demo.model.Korisnik;
@@ -14,11 +19,65 @@ public class KorisnikService {
 	@Autowired
 	KorisnikRepository korisnikRepository;
 
-	public List<Korisnik> findAll() {
-		return korisnikRepository.findAll();
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
+	public Optional<Korisnik> findById(Long id) {
+
+		if (id == null) {
+			return null;
+		}
+
+		return korisnikRepository.findByIdAndAktivan(id, true);
 	}
 
-	public Korisnik save(Korisnik korisnik) {
+	public List<Korisnik> findAll() {
+
+		return korisnikRepository.findByAktivan(true);
+	}
+
+	public Korisnik findAuthenticatedKorisnik() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String currentPrincipalName = authentication.getName();
+
+		return korisnikRepository.findByKorisnickoImeAndAktivan(currentPrincipalName, true).orElse(null);
+	}
+
+	public Korisnik findByKorisnickoIme(String korisnickoIme) {
+		return korisnikRepository.findByKorisnickoImeAndAktivan(korisnickoIme, true).orElse(null);
+	}
+
+	public Korisnik save(Korisnik korisnik) throws Exception {
+
+		if (korisnik == null) {
+			return null;
+		}
+
+		boolean isNew = korisnik.getId() == null || findById(korisnik.getId()).isEmpty();
+
+		//enkripcija lozinke za novog korisnika
+		if (isNew) {
+
+			if (StringUtils.isBlank(korisnik.getLozinka())) {
+				throw new Exception("Lozinka je obavezna za novog korisnika");
+			}
+
+			korisnik.setLozinka(passwordEncoder.encode(korisnik.getLozinka()));
+
+		} else {// lozinka se ne može ažurirati za postojećeg korisnika
+
+			if (StringUtils.isNotBlank(korisnik.getLozinka())) {
+				throw new Exception("Lozinka se ne može ažurirati za postojećeg korisnika");
+			}
+
+			//dohvati i postavi trenutnu lozinku
+			Korisnik existingKorisnik = findById(korisnik.getId()).get();
+			korisnik.setLozinka(existingKorisnik.getLozinka());
+		}
+
+		//postavi aktivan
+		korisnik.setAktivan(true);
+
 		return korisnikRepository.save(korisnik);
 	}
 
